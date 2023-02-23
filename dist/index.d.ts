d76d5d39f9b3d361c27a6d6204a637d215ee7ec1
Mark Hall <markh@untangl.co.uk>
2023-02-23 16:23:17 +0000

    Add dist files

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+import { ExpressAppConfig } from "./middleware/express.app.config";
+import { Oas3AppOptions } from "./middleware/oas3.options";
+import { OpenApiRequestHandler } from 'express-openapi-validator/dist/framework/types';
+export declare function expressAppConfig(definitionPath: string, appOptions: Oas3AppOptions, customMiddlewares?: OpenApiRequestHandler[]): ExpressAppConfig;
